9b79eea06ecc47f44d4a3153cdd15a02e6db7ac3
Anabovo <sousabovoana@gmail.com>
2024-09-23 10:53:31 -0300

    dia 23/05

--- showdomilhao/Modelos/Gerenciador.cs
+++ showdomilhao/Modelos/Gerenciador.cs
@@ -1,3 +1,5 @@
+using showdomilhao;
+
 public class Gerenciador
 {
     List<Questao> ListaQuestoes = new List<Questao>();
@@ -31,7 +33,14 @@ public class Gerenciador
         if (QuestaoCorrente.VerificaResposta(RR))
         {
             await Task.Delay(1000);
+            AdicionaPontuacao(NivelAtual);
+            NivelAtual ++;
             ProximaQuestao();
+        }
+        else
+        {
+            await App.Current.MainPage.DisplayAlert ("Game Over", "Que pena... Você errou!", "OK");
+            Inicializar();
         }
     }
 
@@ -44,4 +53,40 @@ public class Gerenciador
         QuestaoCorrente = ListaQuestoes[numAleat];
         QuestaoCorrente.Desenhar();
     }
+
+    public int Pontuacao{get; private set;}
+    int NivelAtual=0;
+
+    void  Inicializar()
+    {
+        Pontuacao = 0;
+        NivelAtual = 1;
+        ProximaQuestao();
+    }
+
+    void AdicionaPontuacao(int n)
+    {
+        if (n==1)
+          Pontuacao = 1000;
+        else if (n==2)  
+          Pontuacao = 2000;
+        else if (n==3)  
+          Pontuacao = 5000;
+        else if (n==2)  
+          Pontuacao = 10000;
+        else if (n==3)  
+          Pontuacao = 20000;
+        else if (n==2)  
+          Pontuacao = 50000;
+        else if (n==3)  
+          Pontuacao = 100000;
+        else if (n==2)  
+          Pontuacao = 200000;
+        else if (n==3)  
+          Pontuacao = 500000;
+         else if (n==3)  
+          Pontuacao = 1000000;
+    }
+
+
 }
--- showdomilhao/TelaPergunta.xaml.cs
+++ showdomilhao/TelaPergunta.xaml.cs
@@ -14,4 +14,26 @@ public partial class TelaPergunta : ContentPage
 	{
 		gerenciador.VerificaResposta(1);
 	}
+
+	void Resposta02Clicado(object sender, EventArgs e)	
+	{
+		gerenciador.VerificaResposta(1);
+	}
+
+	void Resposta03Clicado(object sender, EventArgs e)	
+	{
+		gerenciador.VerificaResposta(1);
+	}
+
+	void Resposta04Clicado(object sender, EventArgs e)	
+	{
+		gerenciador.VerificaResposta(1);
+	}
+
+	void Resposta05Clicado(object sender, EventArgs e)	
+	{
+		gerenciador.VerificaResposta(1);
+	}
+
+
 }
